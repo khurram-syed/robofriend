78a8d448ea7553617a6a36036868e75b2b86217f
khurram-syed <khurram245@yahoo.com>
2019-09-29 17:33:42 +0100

    Updates

---
+++ precache-manifest.57f65b1b55936ca80424203938fdb6e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c3878f9f4dc224a6b05bc2d18041c7",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "a4a0c64a806aed484bc8",
+    "url": "/robofriend/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "fedb38ad00c362a70398",
+    "url": "/robofriend/static/css/main.2629167b.chunk.css"
+  },
+  {
+    "revision": "a4a0c64a806aed484bc8",
+    "url": "/robofriend/static/js/2.b3f888bd.chunk.js"
+  },
+  {
+    "revision": "fedb38ad00c362a70398",
+    "url": "/robofriend/static/js/main.766cbd4a.chunk.js"
+  },
+  {
+    "revision": "ad7151fe4baf59dddf9a",
+    "url": "/robofriend/static/js/runtime~main.76fe5555.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);

c4973de1890c0b2f6d43d6556c7cb6d552fd1e64
khurram-syed <khurram245@yahoo.com>
2019-09-29 17:58:32 +0100

    updated Card.js and App.css

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Card = ({id,name,email}) =>{
      return (
-         <div className="tc bg-light-blue dib br3 pa1 ma4 grow bw2 shadow-5">
+         <div className="tc bg-light-blue dib br3 pa1 ma2 grow bw2 shadow-5">
              <img alt ="Robot" src ={`https://robohash.org/${id}`} />
              <div>
                  <h2>{name}</h2>
